fddfec98e314b639ad8bf31b2e44e848891d819c
Jan Schulte <jan@work-with-jan.com>
2021-08-19 08:14:10 -0400

    Show all types from a given assembly

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Mono.Cecil;
+using Mono.Cecil.Rocks;
 
 namespace ilspy
 {
@@ -6,7 +8,34 @@ namespace ilspy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("ilspy");
+            if (args.Length < 1)
+            {
+                Console.Error.WriteLine("Expected Assembly path. Got: None");
+                Console.Error.WriteLine("Usage: ilspy <assembly path> [<class.method>]");
+                return;
+            }
+
+            var assemblyPath = args[0];
+
+            var module = ModuleDefinition.ReadModule(assemblyPath);
+            ShowAllTypes(module);
+        }
+
+        private static void ShowAllTypes(ModuleDefinition module)
+        {
+            //Returns a list of all classes that are present in the assembly
+            var allTypes = module.GetTypes();
+
+            foreach (var typeDefinition in allTypes)
+            {
+                Console.WriteLine($"{typeDefinition.Name}");
+                //`typeDefinition.GetMethods()` returns a list of all methods for this class
+                foreach (var methodDefinition in typeDefinition.GetMethods())
+                {
+                    Console.WriteLine($"{typeDefinition}.{methodDefinition.Name}");
+                }
+            }
         }
     }
 }
